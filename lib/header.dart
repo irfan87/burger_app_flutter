b362554daa53e99b4f3c51ac6b3c9625689ea3af
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-27 16:26:00 +0800

    make a simple but catchy looking user info, including the profile picture

--- lib/header.dart
+++ lib/header.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class Header extends StatefulWidget {
   @override
@@ -14,7 +16,7 @@ class _HeaderState extends State<Header> {
     return SliverList(
       delegate: SliverChildListDelegate(
         [
-          Column(
+          Stack(
             children: [
               Container(
                 padding: const EdgeInsets.symmetric(
@@ -43,7 +45,48 @@ class _HeaderState extends State<Header> {
                         CircleAvatar(
                           backgroundColor: Colors.white70,
                           radius: 35.0,
-                        )
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage("images/white_cat.png"),
+                            radius: 30.0,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "John Doe",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(5.0),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20.0),
+                                color: Colors.black54,
+                              ),
+                              child: Text(
+                                "Gold Member",
+                                style: TextStyle(
+                                  color: Colors.yellow[700],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Spacer(),
+                        Text(
+                          "154 \$ CAN",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18.0,
+                          ),
+                        ),
                       ],
                     ),
                   ],
