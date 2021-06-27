40cdf0fffb5f2053e7bde96c9d5f7d2cac5a85e3
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-27 23:42:53 +0800

    appbar the second page which is burger's detail

--- lib/burger_page.dart
+++ lib/burger_page.dart
@@ -8,10 +8,126 @@ class BurgerPage extends StatefulWidget {
 }
 
 class _BurgerPageState extends State<BurgerPage> {
+  Widget chickenBurgerImage = Container(
+    height: 110.0,
+    child: Image.asset("images/chicken_burger.png"),
+  );
+
+  Widget baconCheeseBurgerImage = Container(
+    height: 160.0,
+    child: Image.asset("images/bacon_cheeseburger.png"),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: CircleAvatar(
+              backgroundColor: Colors.white70,
+              radius: 15.0,
+              child: CircleAvatar(
+                backgroundImage: AssetImage("images/white_cat.png"),
+                radius: 13.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 30.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Chicken Burger",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  "Deliver Me BURGER. Fast Delivery & Great Service!",
+                  style: TextStyle(
+                    fontSize: 14.0,
+                    color: Colors.white54,
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Row(
+                  children: [
+                    chickenBurgerImage,
+                    Spacer(),
+                    Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(4.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                          ),
+                          child: Text(
+                            "15.95 \$ CAN",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.white,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.white,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.white,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.white,
+                            ),
+                            Icon(
+                              Icons.star,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
